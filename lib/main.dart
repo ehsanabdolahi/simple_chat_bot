be6a20333abe8254c197e6dd14d042577d223aab
Ehsan <eaehsanea@gmail.com>
2025-02-05 14:17:13 +0330

    creat stringCategory

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:chat_bot/src/constants/categories/string_category.dart';
-import 'package:chat_bot/src/presentation/pages/chat_page.dart';
+import 'package:chat_bot/src/di/locator.dart';
+import 'package:chat_bot/src/features/chat_bot/presentation/screens/chat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupAllDI();
   runApp(
     const ProviderScope(
       child: MyApp(),
--- lib/src/constants/categories/string_category.dart
+++ lib/src/constants/categories/string_category.dart
@@ -1,5 +1,4 @@
 abstract class StringCategory {
   static const appName = 'Simple Chat Bot';
-  static const greetingMessage = "سلام! ممنون از پیامت. چطور میتونم کمکت کنم؟";
-  static const pleaseEnterYourMessage = 'پیام خود را بنویسید...';
+  static const pleaseEnterYourMessage = 'Please enter your message';
 }
